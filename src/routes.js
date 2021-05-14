7e1e93667b257226980a8a812442fe02bc309a6c
Raul B <raulbunicenha@gmail.com>
2021-05-14 12:13:05 -0300

    create /dashboard as a private route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
 const routes = require('express').Router()
+const auth = require('./app/middleware/auth')
 const SessionController = require('./app/controllers/SessionController')
 
 routes.post('/sessions', SessionController.store)
+
+routes.use(auth)
+
+routes.get('/dashboard', (req, res) => {
+  res.status(200).send({ message: 'Hi' })
+})
 
 module.exports = routes
--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -5,13 +5,15 @@ const truncate = require('../utils/truncate')
 const app = require('../../src/app')
 
 describe('Authentication', () => {
-  beforeEach(async () => truncate())
+  beforeEach(async () => {
+    truncate()
 
-  it('should authenticate with valid credentials', async () => {
-    const user = await factory.create('User', {
+    user = await factory.create('User', {
       password: '1231'
     })
+  })
 
+  it('should authenticate with valid credentials', async () => {
     const response = await request(app)
       .post('/sessions')
       .send({
@@ -23,10 +25,6 @@ describe('Authentication', () => {
   })
 
   it('should not authenticate with invalid email', async () => {
-    const user = await factory.create('User', {
-      password: '1231'
-    })
-
     const response = await request(app)
       .post('/sessions')
       .send({
@@ -38,10 +36,6 @@ describe('Authentication', () => {
   })
 
   it('should not authenticate with invalid password', async () => {
-    const user = await factory.create('User', {
-      password: '1231'
-    })
-
     const response = await request(app)
       .post('/sessions')
       .send({
@@ -53,10 +47,6 @@ describe('Authentication', () => {
   })
 
   it('should return jwt token when authenticated', async () => {
-    const user = await factory.create('User', {
-      password: '1231'
-    })
-
     const response = await request(app)
       .post('/sessions')
       .send({
@@ -65,5 +55,28 @@ describe('Authentication', () => {
       })
 
     expect(response.body).toHaveProperty('token')
+  })
+
+  it('should be able to access private routes when authenticated', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer ${user.generateToken()}`)
+
+    expect(response.status).toBe(200)
+  })
+
+  it('should not be able to access private routes without jwt token', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+
+    expect(response.status).toBe(401)
+  })
+
+  it('should not be able to access private routes with an invalid jwt token', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', 'Bearer 432123')
+
+    expect(response.status).toBe(401)
   })
 })
